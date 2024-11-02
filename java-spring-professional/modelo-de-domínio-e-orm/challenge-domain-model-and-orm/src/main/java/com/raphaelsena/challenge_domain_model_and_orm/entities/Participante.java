e253e94781e1e027c90b547802392e25877fd523
raphael sena <rsenares1@gmail.com>
2024-11-01 23:39:14 -0300

    feat: add Participante class

---
+++ java-spring-professional/modelo-de-domínio-e-orm/challenge-domain-model-and-orm/src/main/java/com/raphaelsena/challenge_domain_model_and_orm/entities/Participante.java
@@ -0,0 +1,62 @@
+package com.raphaelsena.challenge_domain_model_and_orm.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    private String nome;
+    private String email;
+
+    public Participante() {}
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participante that)) return false;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
