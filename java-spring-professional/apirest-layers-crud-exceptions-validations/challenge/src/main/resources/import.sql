0c5ed3ebee6f6a938bedde98af7f3b4a97ee6d83
raphael sena <rsenares1@gmail.com>
2025-02-14 16:52:49 -0300

    feat: add ddl for db population

---
+++ java-spring-professional/apirest-layers-crud-exceptions-validations/challenge/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Raphael Sena', '14434272683', 10000.0, '2000-10-31', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Eduarda', '46840564061', 15000.0, '2002-07-09', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Janaina Sena', '31736779001', 1300.0, '1975-03-15', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Humberto Brito', '59217854077', 10250.0, '1970-04-09', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Felipe Sena', '34517878066', 1400.0, '2004-07-25', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Erliana Castro', '23150129028', 1550.0, '1971-02-01', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ranô Cardoso', '63124445031', 2200.0, '1966-02-02', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Maria Gabriela', '60195291000', 2300.0, '1993-10-03', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Jhonny Miller', '08678591056', 10000.0, '1990-10-26', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Wilma de Sena', '59538318030', 10500.0, '1946-02-22', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Regina de Oliveira', '59217854077', 10250.0, '1946-08-04', 5);
